3e9741f204b8d043b96a832f4045cfab0a787550
Emil <emil.zeynalov@psd2html.com>
2016-07-18 13:29:56 +0300

    delete wrong nav

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -38,7 +38,6 @@ AppAsset::register($this);
         'items' => [
             ['label' => 'Home', 'url' => ['/site/index']],
             ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'My Page', 'url' => ['/site/mypage']],
             ['label' => 'Contact', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest ? (
                 ['label' => 'Login', 'url' => ['/site/login']]
